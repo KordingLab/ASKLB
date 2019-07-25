686677643d4f2a517b50eb6048a4124c88b46e99
Tony Liu <tliu0526@gmail.com>
2019-07-25 10:06:50 -0500

    restructure app.py to package

--- asklb/__init__.py
+++ asklb/__init__.py
@@ -1,9 +1,6 @@
-##!/usr/bin/env python3
-## -*- coding: utf-8 -*-
 """
-Created on Mon Apr 22 14:56:58 2019
 
-@author: hanfeishen and tonyliu
+@author: hanfeishen, tonyliu, jessecui
 """
 
 import os
