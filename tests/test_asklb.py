7107d51e428151c50e51314ff95035ba395f88cb
Tony Liu <tliu0526@gmail.com>
2020-05-01 21:36:31 -0500

    rename model utils file

--- tests/test_asklb.py
+++ tests/test_asklb.py
@@ -6,21 +6,19 @@ import pandas as pd
 import numpy as np
 import unittest
 
-import asklb.automl
+import asklb.model_utils
 
-class TestAutoML(unittest.TestCase):
+class TestModelUtils(unittest.TestCase):
 
     def test_transform_features(self):
-        n = 100
-        numeric_col = np.random.choice(2, size=n)
+        numeric_col = np.random.choice(2, size=100)
         df = pd.DataFrame()
         
         df['numeric'] = numeric_col
         df['str_cat'] = numeric_col
         df['str_cat'] = df['str_cat'].map({0: "female", 1: "male"})
-        #df['obj_cat'] = map(lambda x: [] if x > 0 else {}, numeric_col)
 
-        data, feat_types, cat_dict = asklb.automl.process_feat_types(df)
+        data, feat_types, cat_dict = asklb.model_utils.process_feat_types(df)
 
         self.assertEqual(data['str_cat'].dtype, np.int64)
         self.assertEqual(feat_types, ['Numerical', 'Categorical'])
