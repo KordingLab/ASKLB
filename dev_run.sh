578f22af9be1e56302df16d034b2f343c61a8fc3
Tony Liu <tliu0526@gmail.com>
2020-12-11 15:41:21 -0600

    update widget.py and asklb_colab.ipynb for colab hosting

--- dev_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# runs the application in developer mode on the specified port
-
-export FLASK_APP=asklb
-export FLASK_ENV=development
-
-pip install -e .
-flask run --host=0.0.0.0 --port $1
